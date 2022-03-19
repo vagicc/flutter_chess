ea732af798c567b2a96f7b43f88c0cc11880627d
临来笑笑生 <luck@elapse.date>
2022-03-19 22:08:22 +0800

    使用云端服器象棋云库

--- lib/cchess/cc_base.dart
+++ lib/cchess/cc_base.dart
@@ -67,3 +67,70 @@ class Piece {
 
   static bool isBlack(String c) => 'rnbakcp'.contains(c);
 }
+
+class Move {
+  static const InvalidIndex = -1;
+  int from=0, to=0;
+  int fx = 0, fy = 0, tx = 0, ty = 0; // 左上角为坐标原点
+  String captured='';
+  String step = ''; // 'step' is the ucci engine's move-string
+
+  Move(this.from, this.to, {this.captured = Piece.Empty}) {
+    //
+    fx = from % 9;
+    fy = from ~/ 9;
+
+    tx = to % 9;
+    ty = to ~/ 9;
+
+    if (fx < 0 || fx > 8 || fy < 0 || fy > 9) {
+      throw "Error: Invlid Step (from:$from, to:$to)";
+    }
+
+    step = String.fromCharCode('a'.codeUnitAt(0) + fx) + (9 - fy).toString();
+    step += String.fromCharCode('a'.codeUnitAt(0) + tx) + (9 - ty).toString();
+  }
+
+  /// 引擎返回的招法用是 4 个字符表示的，例如 b0c2
+  /// 它的着法基于左下角坐标系
+  /// 用 a ~ i 表示从左到右的 9 列
+  /// 用 0 ~ 9 表示从下到上 10 行
+  /// 因此 b0c2 表示从第 2 列第 1 行移动到第 3 列第 3 行
+  Move.fromEngineStep(String step) {
+    //
+    this.step = step;
+
+    if (!validateEngineStep(step)) {
+      throw "Error: Invlid Step: $step";
+    }
+
+    fx = step[0].codeUnitAt(0) - 'a'.codeUnitAt(0);
+    fy = 9 - (step[1].codeUnitAt(0) - '0'.codeUnitAt(0));
+    tx = step[2].codeUnitAt(0) - 'a'.codeUnitAt(0);
+    ty = 9 - (step[3].codeUnitAt(0) - '0'.codeUnitAt(0));
+
+    from = fx + fy * 9;
+    to = tx + ty * 9;
+
+    captured = Piece.Empty;
+  }
+
+  // 验证引擎返回着法是否是合法的
+  static bool validateEngineStep(String step) {
+    //
+    if (step == null || step.length < 4) return false;
+
+    final fx = step[0].codeUnitAt(0) - 'a'.codeUnitAt(0);
+    final fy = 9 - (step[1].codeUnitAt(0) - '0'.codeUnitAt(0));
+    if (fx < 0 || fx > 8 || fy < 0 || fy > 9) return false;
+
+    final tx = step[2].codeUnitAt(0) - 'a'.codeUnitAt(0);
+    final ty = 9 - (step[3].codeUnitAt(0) - '0'.codeUnitAt(0));
+    if (tx < 0 || tx > 8 || ty < 0 || ty > 9) return false;
+
+    return true;
+  }
+}
+
+/// 对战结果：未决、赢、输、和
+enum BattleResult { Pending, Win, Lose, Draw }
